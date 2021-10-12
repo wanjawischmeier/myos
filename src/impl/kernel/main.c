7f45ea800f476351c9e526d0d9a686bb4cf4b515
wanjawischmeier <69650445+wanjawischmeier@users.noreply.github.com>
2021-10-12 19:07:29 +0200

    updated build and print

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -3,5 +3,6 @@
 void kernel_main() {
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("Welcome to our 64-bit kernel!");
+    print_str("Welcome to this 64-bit kernel!\n");
+    print_str("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
 }
--- src/impl/x86_64/print.c
+++ src/impl/x86_64/print.c
@@ -81,3 +81,7 @@ void print_str(char* str) {
 void print_set_color(uint8_t foreground, uint8_t background) {
     color = foreground + (background << 4);
 }
+
+void getch() {
+    
+}
